09adfc5cad5fa804c150b81a96aa11aeba7c8f8b
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2018-05-24 20:34:15 +0200

    test(Calcul Distance): Add a test suite to the calcul distance method

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 module.exports.sum = function sum (a, b) {
     return a + b;
-}
+};
+
+/**
+ * I need a method to build a grid from 2 geolocations.
+ * @param coordStart The up left corner of the rectangle
+ * @param coordEnd The bottom right corner of the rectangle
+ * @return A grid of square
+ */
+// TODO
+
+/**
+ * Geographic coordinate sample: 48.815295, 2.251483 (Latitude, Longitude)
+ * The structure to contain coordinate will be:
+ * {
+ *     lat: 48.815295,
+ *     lon: 2.251483
+ * }
+ * __Question ?__ Should i use a specialized object ?
+ * I need a method to get the distance between tow geographic coordinate.
+ */
+module.exports.calculDistance = function calculDistance (coordA, coordB) {
+    return 0;
+};
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,35 @@
 const expect = require('expect.js');
-const { sum } = require('./index.js');
+const {
+    sum,
+    calculDistance
+} = require('./index.js');
 
 describe(`The method 'sum',`, () => {
     it(`Should return 3 given 1 and 2 as inputs.`, () => {
         expect(sum(1, 2)).to.equal(3);
+    });
+});
+
+describe(`The method 'calculDistance',`, () => {
+    const suite = [
+        {
+            coordA: {
+                lat: 48.812920,
+                lon: 2.207042
+            },
+            coordB: {
+                lat: 48.803762,
+                lon: 2.278886
+            },
+            expected: 5359
+        }
+    ];
+
+    suite.forEach(test => {
+        it(`Should return ${test.expected} for the given coordinates.`, () => {
+            const dist = calculDistance(test.coordA, test.coordB);
+
+            expect(dist).to.equal(test.expected);
+        });
     });
 });
