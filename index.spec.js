8d60c95a6c3d04ccc20c279e679d1857695369cd
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2018-05-24 20:47:51 +0200

    test(Distance): Add test case

--- index.spec.js
+++ index.spec.js
@@ -22,11 +22,44 @@ describe(`The method 'calculDistance',`, () => {
                 lon: 2.278886
             },
             expected: 5359
+        },
+        {
+            coordA: {
+                lat: 48.827182,
+                lon: 2.235062
+            },
+            coordB: {
+                lat: 48.827179,
+                lon: 2.235075
+            },
+            expected: 1
+        },
+        {
+            coordA: {
+                lat: 48.829727,
+                lon: 2.249323
+            },
+            coordB: {
+                lat: 48.829531,
+                lon: 2.249522
+            },
+            expected: 26
+        },
+        {
+            coordA: {
+                lat: 48.847929,
+                lon: 2.205667
+            },
+            coordB: {
+                lat: 48.847463,
+                lon: 2.211067
+            },
+            expected: 399
         }
     ];
 
     suite.forEach(test => {
-        it(`Should return ${test.expected} for the given coordinates.`, () => {
+        it(`Should return ${test.expected}m for the given coordinates.`, () => {
             const dist = calculDistance(test.coordA, test.coordB);
 
             expect(dist).to.equal(test.expected);
