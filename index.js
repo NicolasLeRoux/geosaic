919c2816115fc2f3b3f6895dd29813a2bae3beef
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-27 21:37:54 +0200

    feat(Grid): Add method to build a grid

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const {
+	calculNextLatitude,
+	calculNextLongitude
+} = require('./lib/math.js');
+
 /**
  * Method to build a grid from 2 geolocations.
  * @param coordStart The up left corner of the rectangle
@@ -5,11 +10,25 @@
  * @param length The length of the square side
  * @return A grid of square
  */
-module.exports.buildGrid = function buildGrid (coordStart, coordEnd, length) {
-	/**
-	 * I need to have a reference point, so my grid will always be
-	 * the same. But, i need to calculate the step in order to move
-	 * from one step to another.
-	 */
-	return [];
+module.exports.buildGrid = function buildGrid (coordStart, coordEnd, step) {
+	const result = [];
+	let lat = coordStart.lat,
+		lon = coordStart.lon,
+		coord;
+
+	while (lat >= coordEnd.lat) {
+		while (lon <= coordEnd.lon) {
+			coord = {
+				lat,
+				lon
+			};
+			result.push(coord);
+
+			lon = calculNextLongitude(coord, step);
+		}
+		lat = calculNextLatitude(coord, step);
+		lon = coordStart.lon;
+	}
+
+	return result;
 }
