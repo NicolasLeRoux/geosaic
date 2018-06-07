ccada01c8b7582a06787411f03d947ce6ed41ae1
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2018-06-07 20:32:40 +0200

    refactor(GeoTile Utils): Move function about neighbors in GeoTile module

--- index.js
+++ index.js
@@ -26,163 +26,19 @@ const {
 const FakeGeoService = require('./lib/fake-geo.service.js');
 const {
 	buildGeoTile,
-    splitGeoTile
+    splitGeoTile,
+	getNeighbors,
+	isAdjacentLatitude,
+	isAdjacentLongitude,
+	isTopCornerInsideTileBandLatitude,
+	isBottomCornerInsideTileBandLatitude,
+	isLeftCornerInsideTileBandLongitude,
+	isRightCornerInsideTileBandLongitude,
+	isTileBandLatitudeInsideBiggerTile,
+	isTileBandLongitudeInsideBiggerTile,
+	isSmallerTileInsideTileBandLatitude,
+	isSmallerTileInsideTileBandLongitude
 } = require('./lib/geo-tile.utils.js');
-
-/**
- * Util to return an array of neighbors for a given GeoTile.
- * @param geoTiles An array of all GeoTile to compute
- * @param current The GoeTile to analyze
- * @return The array of neighbors
- */
-const getNeighbors = function getNeighbors (geoTiles, current) {
-	return geoTiles.filter(tile => {
-		// Remove current element
-		if (current.id === tile.id) {
-			return false;
-		}
-
-		// Tiles on the same longitude
-		if (isAdjacentLatitude(current, tile)) {
-			if (isLeftCornerInsideTileBandLongitude(current, tile) ||
-				isRightCornerInsideTileBandLongitude(current, tile) ||
-				isTileBandLongitudeInsideBiggerTile(current, tile) ||
-				isSmallerTileInsideTileBandLongitude(current, tile)) {
-					return true;
-				}
-		}
-
-		// Tiles on the same latitude
-		if (isAdjacentLongitude(current, tile)) {
-			if (isTopCornerInsideTileBandLatitude(current, tile) ||
-				isBottomCornerInsideTileBandLatitude(current, tile) ||
-				isTileBandLatitudeInsideBiggerTile(current, tile) ||
-				isSmallerTileInsideTileBandLatitude(current, tile)) {
-					return true;
-				}
-		}
-
-		return false;
-	});
-};
-
-/**
- * Util to evaluate if the given tiles have a side on an adjacent latitude.
- * @param tileA The first tile
- * @param tileB The second tile
- * @return A state for the test
- */
-const isAdjacentLatitude = function isAdjacentLatitude (tileA, tileB) {
-	return tileA.coords[0].lat === tileB.coords[3].lat ||
-		tileA.coords[3].lat === tileB.coords[0].lat;
-};
-
-/**
- * Util to evaluate if the given tiles have a side on an adjacent longitude.
- * @param tileA The first tile
- * @param tileB The second tile
- * @return A state for the test
- */
-const isAdjacentLongitude = function isAdjacentLongitude (tileA, tileB) {
-	return tileA.coords[0].lon === tileB.coords[1].lon ||
-		tileA.coords[1].lon === tileB.coords[0].lon;
-};
-
-/**
- * Util to evaluate if the target tile has a top corner inside the latitude band
- * of the reference tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isTopCornerInsideTileBandLatitude = function isTopCornerInsideTileBandLatitude (refTile, targetTile) {
-	return refTile.coords[0].lat <= targetTile.coords[0].lat &&
-		refTile.coords[3].lat > targetTile.coords[0].lat;
-};
-
-/**
- * Util to evaluate if the target tile has a bottom corner inside the latitude band
- * of the reference tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isBottomCornerInsideTileBandLatitude = function isBottomCornerInsideTileBandLatitude (refTile, targetTile) {
-	return refTile.coords[0].lat < targetTile.coords[3].lat &&
-		refTile.coords[3].lat >= targetTile.coords[3].lat;
-};
-
-/**
- * Util to evaluate if the target tile has a left corner inside the longitude band
- * of the reference tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isLeftCornerInsideTileBandLongitude = function isLeftCornerInsideTileBandLongitude (refTile, targetTile) {
-	return refTile.coords[0].lon <= targetTile.coords[0].lon &&
-		refTile.coords[1].lon > targetTile.coords[0].lon;
-};
-
-/**
- * Util to evaluate if the target tile has a right corner inside the longitude band
- * of the reference tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isRightCornerInsideTileBandLongitude = function isRightCornerInsideTileBandLongitude (refTile, targetTile) {
-	return refTile.coords[0].lon < targetTile.coords[1].lon &&
-		refTile.coords[1].lon >= targetTile.coords[1].lon;
-};
-
-/**
- * Util to evaluate if the target tile has the latitude band of the reference tile
- * inside it.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isTileBandLatitudeInsideBiggerTile = function isTileBandLatitudeInsideBiggerTile (refTile, targetTile) {
-	return refTile.coords[0].lat <= targetTile.coords[0].lat &&
-		refTile.coords[3].lat >= targetTile.coords[3].lat;
-};
-
-/**
- * Util to evaluate if the target tile has the longitude band of the reference tile
- * inside it.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isTileBandLongitudeInsideBiggerTile = function isTileBandLongitudeInsideBiggerTile (refTile, targetTile) {
-	return refTile.coords[0].lon <= targetTile.coords[0].lon &&
-		refTile.coords[1].lon >= targetTile.coords[1].lon;
-};
-
-/**
- * Util to evaluate if the target tile is inside the latitude band of the reference
- * tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isSmallerTileInsideTileBandLatitude = function isSmallerTileInsideTileBandLatitude (refTile, targetTile) {
-	return refTile.coords[0].lat >= targetTile.coords[0].lat &&
-		refTile.coords[3].lat <= targetTile.coords[3].lat;
-};
-
-/**
- * Util to evaluate if the target tile is inside the longitude band of the reference
- * tile.
- * @param refTile The first tile used to simulate the band
- * @param targetTile The second tile, aka tile to evaluate
- * @return A state for the test
- */
-const isSmallerTileInsideTileBandLongitude = function isSmallerTileInsideTileBandLongitude (refTile, targetTile) {
-	return refTile.coords[0].lon >= targetTile.coords[0].lon &&
-		refTile.coords[1].lon <= targetTile.coords[1].lon;
-};
 
 /**
  * Runner !
@@ -248,16 +104,5 @@ const run = function run (coordStart, coordEnd, step) {
 };
 
 module.exports = {
-	getNeighbors,
-	isAdjacentLatitude,
-	isAdjacentLongitude,
-	isTopCornerInsideTileBandLatitude,
-	isBottomCornerInsideTileBandLatitude,
-	isLeftCornerInsideTileBandLongitude,
-	isRightCornerInsideTileBandLongitude,
-	isTileBandLatitudeInsideBiggerTile,
-	isTileBandLongitudeInsideBiggerTile,
-	isSmallerTileInsideTileBandLatitude,
-	isSmallerTileInsideTileBandLongitude,
 	run
 };
